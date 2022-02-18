92fe55d5ce37f6f19803e8cf84c952c80af09575
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2022-02-18 16:09:24 +0100

    WalletWasabi.Backend: Add tests for sizes of P2WPKH scripts

---
+++ WalletWasabi.Tests/UnitTests/ConstantsTests.cs
@@ -0,0 +1,15 @@
+using WalletWasabi.Helpers;
+using Xunit;
+using WalletWasabi.Helpers;
+
+namespace WalletWasabi.Tests.UnitTests;
+
+public class ConstantsTests
+{
+	[Fact]
+	public void True()
+	{
+		Assert.Equal(Constants.P2wpkhInputMaximumVirtualSize, 68);
+		Assert.Equal(Constants.P2wpkhOutputVirtualSize, 31);
+	}
+}
