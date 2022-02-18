d38343ea614fde289a9810295b0f18ed2fe8ab79
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2022-02-18 16:09:59 +0100

    WalletWasabi.Backend: Add tests for sizes of P2TR scripts

--- WalletWasabi.Tests/UnitTests/ConstantsTests.cs
+++ WalletWasabi.Tests/UnitTests/ConstantsTests.cs
@@ -10,5 +10,7 @@ public class ConstantsTests
 	{
 		Assert.Equal(68, Constants.P2wpkhInputMaximumVirtualSize);
 		Assert.Equal(31, Constants.P2wpkhOutputVirtualSize);
+		Assert.Equal(58, Constants.P2trInputMaximumVirtualSize);
+		Assert.Equal(43, Constants.P2trOutputVirtualSize);
 	}
 }
