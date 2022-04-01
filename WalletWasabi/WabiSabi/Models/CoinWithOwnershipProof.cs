698e2ffdbaa89cf47da0cc04567ed464a6cb55a5
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2022-04-01 15:44:36 +0200

    Introduce CoinWithOwnershipProof

---
+++ WalletWasabi/WabiSabi/Models/CoinWithOwnershipProof.cs
@@ -0,0 +1,20 @@
+using NBitcoin;
+using System.ComponentModel.DataAnnotations;
+using WalletWasabi.Crypto;
+
+namespace WalletWasabi.WabiSabi.Models;
+
+public class CoinWithOwnershipProof : Coin
+{
+	public OwnershipProof OwnershipProof { get; set; }
+
+	public CoinWithOwnershipProof(Coin coin, OwnershipProof ownershipProof) : base(coin.Outpoint, coin.TxOut)
+	{
+		OwnershipProof = ownershipProof;
+	}
+
+	public CoinWithOwnershipProof(OutPoint outpoint, TxOut txOut, OwnershipProof ownershipProof) : base(outpoint, txOut)
+	{
+		OwnershipProof = ownershipProof;
+	}
+}
