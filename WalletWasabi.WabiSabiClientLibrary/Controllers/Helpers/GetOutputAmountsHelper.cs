6876e273be8b6e89bb830e35ef1481c045f04b80
MeinMunzen <dev@null.com>
2022-04-11 11:58:44 +0200

    WSCL: Implement get-output-amounts

---
+++ WalletWasabi.WabiSabiClientLibrary/Controllers/Helpers/GetOutputAmountsHelper.cs
@@ -0,0 +1,36 @@
+using NBitcoin;
+using System.Collections.Generic;
+using System.Linq;
+using WalletWasabi.Extensions;
+using WalletWasabi.WabiSabiClientLibrary.Models;
+using WalletWasabi.WabiSabi.Client;
+using WalletWasabi.WabiSabi.Models;
+using WabiSabi.Crypto.Randomness;
+
+namespace WalletWasabi.WabiSabiClientLibrary.Controllers.Helpers;
+
+public class GetOutputAmountsHelper
+{
+	public static GetOutputAmountsResponse GetOutputAmounts(GetOutputAmountsRequest request, WasabiRandom random)
+	{
+		if (request.InputSize != ScriptType.Taproot.EstimateInputVsize())
+		{
+			throw new Exception("Incorrect input size. Only taproot is supported.");
+		}
+
+		if (request.OutputSize != ScriptType.Taproot.EstimateOutputVsize())
+		{
+			throw new Exception("Incorrect output size. Only taproot is supported");
+		}
+
+		AmountDecomposer decomposer = new(request.MiningFeeRate, request.AllowedOutputAmounts, request.AvailableVsize, true, random, false);
+
+		IEnumerable<Money> myInputCoinEffectiveValues = request.InternalAmounts.Select(x => Money.FromUnit(x, MoneyUnit.Satoshi));
+		IEnumerable<Money> othersInputCoinEffectiveValues = request.ExternalAmounts.Select(x => Money.FromUnit(x, MoneyUnit.Satoshi));
+
+		IEnumerable<Money> response = decomposer.Decompose(myInputCoinEffectiveValues, othersInputCoinEffectiveValues).Select(x => x.Amount);
+		long[] result = response.Select(x => x.Satoshi).ToArray();
+
+		return new GetOutputAmountsResponse(result);
+	}
+}
