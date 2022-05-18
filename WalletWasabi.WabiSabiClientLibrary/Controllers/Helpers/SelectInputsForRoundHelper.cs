276a7b4d24bd24d93bcd1a2caafc36f7c8433151
MeinMunzen <dev@null.com>
2022-05-18 12:54:27 +0200

    WSCL: Implement select-inputs-for-round

---
+++ WalletWasabi.WabiSabiClientLibrary/Controllers/Helpers/SelectInputsForRoundHelper.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using NBitcoin;
+using WabiSabi.Crypto.Randomness;
+using WalletWasabi.Blockchain.TransactionOutputs;
+using WalletWasabi.WabiSabi.Backend.Rounds;
+using WalletWasabi.WabiSabi.Client;
+using WalletWasabi.WabiSabiClientLibrary.Models;
+using WalletWasabi.WabiSabiClientLibrary.Models.SelectInputsForRound;
+
+namespace WalletWasabi.WabiSabiClientLibrary.Controllers.Helpers;
+
+public class SelectInputsForRoundHelper
+{
+	public static SelectInputsForRoundResponse SelectInputsForRound(SelectInputsForRoundRequest request, WasabiRandom random)
+	{
+		UtxoSelectionParameters utxoSelectionParameters = new(request.AllowedInputAmounts, request.AllowedOutputAmounts, request.CoordinationFeeRate, request.MiningFeeRate, request.AllowedInputTypes.ToImmutableSortedSet());
+		ImmutableList<Utxo> coins = CoinJoinCoinSelector.SelectCoinsForRound<Utxo>(request.Utxos, utxoSelectionParameters, request.ConsolidationMode, request.AnonScoreTarget, request.SemiPrivateThreshold, request.LiquidityClue, random);
+
+		Dictionary<ISmartCoin, int> coinIndices = request.Utxos
+			.Select((x, i) => ((ISmartCoin)x, i))
+			.ToDictionary(x => x.Item1, x => x.Item2);
+
+		// Find corresponding indices for the found coins.
+		int[] indices = coins.Select(c => coinIndices[c]).ToArray();
+
+		return new SelectInputsForRoundResponse(indices);
+	}
+}
