e2022ea51b4414b25bf69a96f46bbfca4e8f9e14
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2022-12-01 15:38:10 +0100

    WCL: Translate exceptions

--- WalletWasabi.WabiSabiClientLibrary/Controllers/Controller.cs
+++ WalletWasabi.WabiSabiClientLibrary/Controllers/Controller.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Mvc;
+using WalletWasabi.WabiSabiClientLibrary.Filters;
 
 namespace WalletWasabi.WabiSabiClientLibrary.Controllers;
 
 [ApiController]
+[ExceptionTranslateFilter]
 [Produces("application/json")]
 public class Controller : ControllerBase
 {
