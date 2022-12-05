bafd10a453fa41c3ed82b00cf6a6ec42ccd660d0
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2022-12-05 16:32:27 +0100

    WCL: Rename methods

--- WalletWasabi.WabiSabiClientLibrary/Controllers/Controller.cs
+++ WalletWasabi.WabiSabiClientLibrary/Controllers/Controller.cs
@@ -25,7 +25,7 @@ public class Controller : ControllerBase, IDisposable
 		return new GetVersionResponse(Global.Version);
 	}
 
-	[HttpPost("analyze-transaction")]
+	[HttpPost("get-anonymity-scores")]
 	public AnalyzeTransactionsResponse CreateRequestAsync(AnalyzeTransactionsRequest request)
 	{
 		return AnalyzeTransactionsHelper.AnalyzeTransactions(request);
@@ -35,7 +35,7 @@ public class Controller : ControllerBase, IDisposable
 	/// Given a set of unspent transaction outputs, choose a subset of the outputs that are best to register in a single CoinJoin round according to the given strategy.
 	/// </summary>
 	/// <seealso cref="CoinJoinClient.SelectCoinsForRound"/>
-	[HttpPost("select-utxo-for-round")]
+	[HttpPost("select-inputs-for-round")]
 	public SelectUtxoForRoundResponse SelectUtxoForRound(SelectUtxoForRoundRequest request)
 	{
 		return SelectUtxoForRoundHelper.Select(request);
@@ -45,25 +45,25 @@ public class Controller : ControllerBase, IDisposable
 	/// Given a set of effective input amounts registered by a participant and a set of effective input amounts
 	/// registered by other participants, decompose the amounts registered by the participant into output amounts.
 	/// </summary>
-	[HttpPost("decompose-amounts")]
+	[HttpPost("get-outputs-amounts")]
 	public DecomposeAmountsResponse DecomposeAmounts(DecomposeAmountsRequest request)
 	{
 		return DecomposeAmountsHelper.Decompose(request);
 	}
 
-	[HttpPost("create-zero-amount-credential-request")]
+	[HttpPost("get-zero-credential-requests")]
 	public CreateZeroAmountCredentialRequestResponse CreateZeroAmountCredentialRequestAsync(CreateZeroAmountCredentialRequestRequest request)
 	{
 		return CredentialHelper.CreateZeroAmountCredentialRequest(request, _secureRandom);
 	}
 
-	[HttpPost("create-credential-request")]
+	[HttpPost("get-real-credential-requests")]
 	public CreateCredentialRequestResponse CreateCredentialRequestAsync(CreateCredentialRequestRequest request)
 	{
 		return CredentialHelper.CreateCredentialRequest(request, _secureRandom);
 	}
 
-	[HttpPost("handle-credential-response")]
+	[HttpPost("get-credentials")]
 	public HandleCredentialResponseResponse HandleCredentialResponseAsync(HandleCredentialResponseRequest request)
 	{
 		return CredentialHelper.HandleCredentialResponse(request, _secureRandom);
