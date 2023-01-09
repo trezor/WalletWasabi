1313bb4653bbea6406343d74b51a63893b5250fe
Ondřej Vejpustek <ondrej.vejpustek@satoshilabs.com>
2023-01-09 15:55:07 +0100

    fixup! WCL: Implement select-inputs-for-round

--- WalletWasabi.WabiSabiClientLibrary/Models/SelectInputsForRound/Utxo.cs
+++ WalletWasabi.WabiSabiClientLibrary/Models/SelectInputsForRound/Utxo.cs
@@ -11,8 +11,7 @@ namespace WalletWasabi.WabiSabiClientLibrary.Models.SelectInputsForRound;
 /// <param name="Amount">Value of the UTXO in Bitcoin (not effective value).</param>
 /// <param name="ScriptType">UTXO's script type.</param>
 /// <param name="AnonymitySet">Anonymity set assigned to the UTXO.</param>
-/// <param name="LastCoinjoinTimestamp">The parameter is not currently needed in seconds.</param>
-public record Utxo(OutPoint Outpoint, Money Amount, ScriptType ScriptType, double AnonymitySet, long LastCoinjoinTimestamp) : ISmartCoin
+public record Utxo(OutPoint Outpoint, Money Amount, ScriptType ScriptType, double AnonymitySet) : ISmartCoin
 {
 	[JsonIgnore]
 	public uint Index => Outpoint.N;
